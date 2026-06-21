42b57ef167cce61d10e950b6bf6c06d16f942969
lucas <lucasmimi21110@gmail.com>
2026-06-21 13:17:45 +0200

    Fix: bouton de déconnexion et onglet event inscrit dans la page de profil

--- EZvents/src/Controller/ProfilController.php
+++ EZvents/src/Controller/ProfilController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UserRepository;
+use App\Repository\EventRepository;
 
 final class ProfilController extends AbstractController
 {
     #[Route('/profil/{pseudo}', name: 'app_profil')]
-    public function index(string $pseudo,UserRepository $userRepository): Response
+    public function index(string $pseudo, UserRepository $userRepository, EventRepository $eventRepository): Response
     {
         $user = $userRepository->findOneBy(['pseudo' => $pseudo]);
 
@@ -22,9 +23,18 @@ final class ProfilController extends AbstractController
             return $event->isArchived() === false;
         });
 
+        $registeredEvents = $eventRepository->createQueryBuilder('e')
+            ->join('e.participants', 'p')
+            ->where('p = :user')
+            ->andWhere('e.isArchived = false')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+
         return $this->render('profil/index.html.twig', [
             'user' => $user,
             'events' => $myEvents,
+            'registeredEvents' => $registeredEvents,
         ]);
     }
 }
